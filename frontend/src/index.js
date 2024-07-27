30878b68766e63b628f46c203860468a7c5e5fe7
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-26 20:59:50 -0500

    modified index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
-        <App />
+        <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+            <React.StrictMode >
+                <App />
+            </React.StrictMode>
+        </GoogleOAuthProvider>
     </Router>
 );
