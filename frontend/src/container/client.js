bbcc75f97e4094b157fed63b3f808085e2b6dfb3
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-29 21:35:54 -0500

    updated client

--- frontend/src/container/client.js
+++ frontend/src/container/client.js
@@ -1,13 +1,15 @@
-import { createClient } from '@sanity/client'
-import { imageUrlBuilder } from '@sanity/image-url'
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-export const client = createClient({
-    projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
-    dataset: 'production',
-    apiVersion: '2024-08-24',
-    useCdn: true,
-    token: `${process.env.REACT_APP_SANITY_TOKEN}`,
+const client = createClient({
+  projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: true,
+  token: `${process.env.REACT_APP_SANITY_TOKEN}`,
 });
 
 const builder = imageUrlBuilder(client);
-export const urlFor = (source) => builder.image(source)
+
+export const urlFor = (source) => builder.image(source);
+export default client;
