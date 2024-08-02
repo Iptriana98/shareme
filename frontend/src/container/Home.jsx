b5017e81327ec56effd825f7733b99a33427b535
Ibrahin Perez <iptrianaa@gmail.com>
2024-08-02 17:14:23 -0500

    created side bar in home component

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -3,7 +3,7 @@ import { HiMenu } from 'react-icons/hi'
 import { AiFillCloseCircle } from 'react-icons/ai'
 import { Link, Route, Routes } from 'react-router-dom'
 
-import { Sidebar, UserProfile } from '../components'
+import { SideBar, UserProfile } from '../components'
 import Pins from './Pins'
 import { userQuery } from '../utils/data'
 import { client } from '../client'
@@ -11,8 +11,41 @@ import logo from '../assets/logo.png'
 
 
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.sub)
+    client.fetch(query).then((data) => {
+      setUser(data[0])
+    })
+  }, [])
+
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world! Home</h1>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <SideBar user={user && user}/>
+      </div>
+
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
+        <Link to='/'>
+          <img src={logo} alt="logo" className='w-28' />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="logo" className='w-28' />
+        </Link>
+        {toggleSidebar && (
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+            <div className='absolute w-full flex justify-end items-center p-2'>
+              <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+            </div>
+            <SideBar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
