7bb75b01af47e495340edd05b8417206b51980d8
Ibrahin Perez <iptrianaa@gmail.com>
2024-08-05 04:14:04 -0500

    updated side bar component

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,6 +3,7 @@ import { NavLink, Link } from 'react-router-dom'
 import {RiHomeFill} from 'react-icons/ri'
 import {IoIosArrowForward} from 'react-icons/io'
 import logo from '../assets/logo.png'
+import { categories } from '../utils/data'
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
@@ -11,16 +12,6 @@ const Sidebar = ({ closeToggle, user }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   };
-
-  const categories = [
-    { name: 'Animals' },
-    { name: 'Wallpapers' },
-    { name: 'Photography' },
-    { name: 'Gaming' },
-    { name: 'Sports' },
-    { name: 'Coding' },
-    { name: 'Music' },
-  ];
 
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
@@ -50,7 +41,7 @@ const Sidebar = ({ closeToggle, user }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
-              {/* <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" /> */}
+              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
               {category.name}
             </NavLink>
           ))}
