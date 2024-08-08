ec102b20365e69f20b3139e526e374a97d9f09c7
Ibrahin Perez <iptrianaa@gmail.com>
2024-08-07 20:45:45 -0500

    modified app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {React, useEffect} from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom'
 
 import Login from './components/Login'
 import Home from './container/Home'
 
 const App = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'))
+        if (user) {
+            navigate('/')
+        } else {
+            navigate('/login')
+        }
+    }, [])
+
     return (
         <Routes>
             <Route path="login" element={<Login />} />
